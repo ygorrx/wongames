00fe3cc6fe9ec3185631fa68c7157d1e0ca6e519
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-09 23:54:22 -0300

    game service

--- api/src/api/game/services/game.js
+++ api/src/api/game/services/game.js
@@ -5,5 +5,18 @@
  */
 
 const { createCoreService } = require("@strapi/strapi").factories;
+const axios = require("axios");
 
-module.exports = createCoreService("api::game.game");
+module.exports = createCoreService("api::game.game", ({ strapi }) => ({
+  populate: async (params) => {
+    const cat = await strapi.services.category.find({ name: "Action" });
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(cat);
+    console.log(products[0]);
+  },
+}));
